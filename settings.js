9aa9dbb9dd12d43c8397c97b4acc91fcb1f29a50
sandeep12aaap <sandeepbandara16@gmail.com>
2026-04-14 22:12:09 +0530

    Set default SESSION_ID in config.jsYassiya

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=c193152612f1effd' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? "800" : process.env.PORT,
+SESSION_NAME: process.env.PORT === undefined ? "sandeep" : process.env.SESSION_NAME,
 };
